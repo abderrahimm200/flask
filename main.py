801498b7c122c943bdd63e57da92ae9373eeff68
Abderrahim Abdellaoui <65556094+abderrahimm200@users.noreply.github.com>
2023-12-07 18:25:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ def convert_to_pdf():
     html_content = request.form.get('html_content')
 
     try:
+        # Ensure the HTML is interpreted as UTF-8
+        html_content = html_content.encode('utf-8').decode('utf-8')
+
         # Convert HTML to PDF
         pdf_data = pdfkit.from_string(html_content, False)
-        return pdf_data, 200, {'Content-Type': 'application/pdf'}
+        return pdf_data, 200, {'Content-Type': 'application/pdf; charset=UTF-8'}
     except Exception as e:
         # Log the error
         app.logger.error(f'wkhtmltopdf error: {str(e)}')
